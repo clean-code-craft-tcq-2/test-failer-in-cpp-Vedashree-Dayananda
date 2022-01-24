770ba240f0b35f8ca3e1dd2c8051fa5584cf1016
Vedashree D <90906584+Vedashree-Dayananda@users.noreply.github.com>
2022-01-24 12:30:56 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <assert.h>
+#include <string.h>
+
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+std::string formatColorPairwithId(int majorColorId , int minorColorId) {
+    
+    
+}
 
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
+            std::string ColorPair_withId = formatColorPairwithId(i,j);
             std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
         }
     }
