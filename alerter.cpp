7a8ab757d2828e1db32c3ad7a565784c3a5885b2
Vedashree D <90906584+Vedashree-Dayananda@users.noreply.github.com>
2022-02-01 23:46:01 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -1,32 +1,27 @@
 #include <iostream>
 #include <assert.h>
 
-#define MINTHRESHOLDTEMPERATURE 36.1
-#define MAXTHRESHOLDTEMPERATURE 37.2
-
 int alertFailureCount = 0;
-int networkAlertCount = 0;
 
 
-int networkAlertStub(float celcius) {
+int networkAlertStubOK(float celcius) {
     std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
-    // Return 200 for ok
-    // Return 500 for not-ok
-    if ((celcius <= MINTHRESHOLDTEMPERATURE) && (celcius >= MAXTHRESHOLDTEMPERATURE))
-    {
+    // Network stub to always return true 
 	    return 200;
-    }
-    networkAlertCount++;
-    return 500;
 }
 
+int networkAlertStubNOK(float celcius) {
+	std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
+	// Network stub to always return false
+	return 500;
+}
 int networkAlert(float celcius) {
+	//Real network alert
     std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
     if ((celcius <= MINTHRESHOLDTEMPERATURE) && (celcius >= MAXTHRESHOLDTEMPERATURE))
     {
 	    return 200;
     }
-    networkAlertCount++;
     return 500;
 }
 
@@ -36,26 +31,27 @@ float farenheitToCelciusConverter(float farenheit)
     return celcius;
 }
 
-void alertInCelcius(float farenheit, int(*networkAlerter)(float)) {
+void alertInCelcius(float farenheit, int(*networkAlerterFunc)(float)) {
     float celcius = farenheitToCelciusConverter(farenheit);
-    int returnCode = networkAlerter(celcius);
+    int returnCode = networkAlerterFunc(celcius);
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+        alertFailureCount++;
     }
 }
 
 int main() {
-    alertInCelcius(400.5, &networkAlertStub);
-    assert(alertFailureCount == networkAlertCount);
-    alertInCelcius(303.6, &networkAlertStub);
-    assert(alertFailureCount == networkAlertCount);
+    alertInCelcius(400.5, &networkAlertStubOK);
+    assert(alertFailureCount == 1);
+    alertInCelcius(303.6, &networkAlertStubOk);
+    assert(alertFailureCount == 2);
+	alertInCelcius(99.0, &networkAlertStubNOk);
+	assert(alertFailureCount == 2);
     alertInCelcius(100.6, &networkAlert);
     std::cout << alertFailureCount << " alerts failed.\n";
-    std::cout << networkAlertCount << " alerts failed.\n";
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
