828f50060c684239ffc478255e43740bf03bfa09
Vedashree D <90906584+Vedashree-Dayananda@users.noreply.github.com>
2022-02-01 23:53:06 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -5,9 +5,9 @@ int alertFailureCount = 0;
 #define MAXTHRESHOLDTEMPERATURE 37.2
 
 int networkAlertStubOK(float celcius) {
-    std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
-    // Network stub to always return true 
-	    return 200;
+	std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
+	// Network stub to always return true 
+	return 200;
 }
 
 int networkAlertStubNOK(float celcius) {
@@ -17,43 +17,45 @@ int networkAlertStubNOK(float celcius) {
 }
 int networkAlert(float celcius) {
 	//Real network alert
-    std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
-    if (celcius >= MAXTHRESHOLDTEMPERATURE))
-    {
-	    return 200;
-    }
-    return 500;
+	std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
+	if (celcius >= MAXTHRESHOLDTEMPERATURE)
+	{
+		return 200;
+	}
+	return 500;
 }
 
 float farenheitToCelciusConverter(float farenheit)
 {
-    float celcius = (farenheit - 32) * 5 / 9;
-    return celcius;
+	float celcius = (farenheit - 32) * 5 / 9;
+	return celcius;
 }
 
 void alertInCelcius(float farenheit, int(*networkAlerterFunc)(float)) {
-    float celcius = farenheitToCelciusConverter(farenheit);
-    int returnCode = networkAlerterFunc(celcius);
-    if (returnCode != 200) {
-        // non-ok response is not an error! Issues happen in life!
-        // let us keep a count of failures to report
-        // However, this code doesn't count failures!
-        // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount++;
-    }
+	float celcius = farenheitToCelciusConverter(farenheit);
+	int returnCode = networkAlerterFunc(celcius);
+	if (returnCode != 200) {
+		// non-ok response is not an error! Issues happen in life!
+		// let us keep a count of failures to report
+		// However, this code doesn't count failures!
+		// Add a test below to catch this bug. Alter the stub above, if needed.
+		alertFailureCount++;
+	}
 }
 
 int main() {
-    alertInCelcius(400.5, &networkAlertStubOK);
-    assert(alertFailureCount == 1);
-    alertInCelcius(303.6, &networkAlertStubOK);
-    assert(alertFailureCount == 2);
-    alertInCelcius(99.0, &networkAlertStubNOK);
-    assert(alertFailureCount == 2);
-    alertInCelcius(200.0, &networkAlertStubNOK);
-    assert(alertFailureCount == 2);
-    alertInCelcius(100.6, &networkAlert);
-    std::cout << alertFailureCount << " alerts failed.\n";
-    std::cout << "All is well (maybe!)\n";
-    return 0;
+	alertInCelcius(400.5, &networkAlertStubNOK);
+	assert(alertFailureCount == 1);
+	alertInCelcius(303.6, &networkAlertStubNOK);
+	assert(alertFailureCount == 2);
+	alertInCelcius(99.0, &networkAlertStubOK);
+	assert(alertFailureCount == 2);
+	alertInCelcius(200.0, &networkAlertStubOK);
+	assert(alertFailureCount == 2);
+	alertInCelcius(200.0, &networkAlertStubNOK);
+	assert(alertFailureCount == 3);
+	alertInCelcius(100.6, &networkAlert);
+	std::cout << alertFailureCount << " alerts failed.\n";
+	std::cout << "All is well (maybe!)\n";
+	return 0;
 }
